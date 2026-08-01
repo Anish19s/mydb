dd80c179eb6c5bcc47d3f55cc440078640a820b3
Anish Seth <anish19seth@gmail.com>
2026-08-01 09:56:58 +0530

    stage2 done

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -1,4 +1,115 @@
 #include "BlockBuffer.h"
-
-#include <cstdlib>
 #include <cstring>
+
+// Constructor
+BlockBuffer::BlockBuffer(int blockNum) {
+    this->blockNum = blockNum;
+}
+
+// Calls the parent class constructor
+RecBuffer::RecBuffer(int blockNum)
+    : BlockBuffer(blockNum) {}
+
+// Load the block header into the argument pointer
+int BlockBuffer::getHeader(struct HeadInfo *head) {
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read the block into the buffer
+    Disk::readBlock(buffer, this->blockNum);
+
+    // Populate the header fields
+    memcpy(&head->blockType, buffer + 0, 4);
+    memcpy(&head->pblock,    buffer + 4, 4);
+    memcpy(&head->lblock,    buffer + 8, 4);
+    memcpy(&head->rblock,    buffer + 12, 4);
+    memcpy(&head->numEntries, buffer + 16, 4);
+    memcpy(&head->numAttrs,   buffer + 20, 4);
+    memcpy(&head->numSlots,   buffer + 24, 4);
+    memcpy(head->reserved,    buffer + 28, 4);
+
+    return SUCCESS;
+}
+
+// Load the record at slotNum into the argument pointer
+int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+
+    HeadInfo head;
+
+    // Read the header
+    getHeader(&head);
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read the block
+    Disk::readBlock(buffer, this->blockNum);
+
+    /*
+        Record layout:
+
+        +---------------------------+
+        | Header (32 bytes)         |
+        +---------------------------+
+        | Slot Map (numSlots bytes) |
+        +---------------------------+
+        | Record 0                  |
+        +---------------------------+
+        | Record 1                  |
+        +---------------------------+
+        | ...                       |
+    */
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+    unsigned char *slotPointer =
+        buffer +
+        HEADER_SIZE +
+        slotCount +
+        (recordSize * slotNum);
+
+    // Copy the record into rec
+    memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
+
+// Store the record at slotNum into the block
+int RecBuffer::setRecord(union Attribute *rec, int slotNum) {
+
+    HeadInfo head;
+
+    // Get header information
+    getHeader(&head);
+
+    int attrCount = head.numAttrs;
+    int slotCount = head.numSlots;
+
+    unsigned char buffer[BLOCK_SIZE];
+
+    // Read block into buffer
+    Disk::readBlock(buffer, this->blockNum);
+
+
+    int recordSize = attrCount * ATTR_SIZE;
+
+
+    unsigned char *slotPointer =
+        buffer +
+        HEADER_SIZE +
+        slotCount +
+        (recordSize * slotNum);
+
+
+    // Copy updated record into block
+    memcpy(slotPointer, rec, recordSize);
+
+
+    // Write updated block back to disk
+    Disk::writeBlock(buffer, this->blockNum);
+
+
+    return SUCCESS;
+}
---
+++ NITCbase/mynitcbase/q1stage2.cpp
@@ -0,0 +1,87 @@
+#include <cstdio>
+#include <cstring>
+
+#include "Buffer/BlockBuffer.h"
+#include "define/constants.h"
+
+int main(int argc, char *argv[]) {
+
+    Disk disk_run;
+
+
+    // Relation Catalog
+    RecBuffer relCatBuffer(RELCAT_BLOCK);
+
+    HeadInfo relCatHeader;
+
+    relCatBuffer.getHeader(&relCatHeader);
+
+
+
+    // Traverse all relations
+    for(int i = 0; i < relCatHeader.numEntries; i++) {
+
+        Attribute relCatRecord[RELCAT_NO_ATTRS];
+
+        relCatBuffer.getRecord(relCatRecord, i);
+
+
+        printf("Relation: %s\n",
+            relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+
+
+        // Traverse Attribute Catalog blocks
+        int attrBlock = ATTRCAT_BLOCK;
+
+
+        while(attrBlock != -1) {
+
+            RecBuffer attrCatBuffer(attrBlock);
+
+            HeadInfo attrCatHeader;
+
+            attrCatBuffer.getHeader(&attrCatHeader);
+
+
+
+            // Traverse attributes in this block
+            for(int j = 0; j < attrCatHeader.numEntries; j++) {
+
+                Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+                attrCatBuffer.getRecord(attrCatRecord, j);
+
+
+
+                // Check if attribute belongs to current relation
+                if(strcmp(
+                    attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal,
+                    relCatRecord[RELCAT_REL_NAME_INDEX].sVal
+                ) == 0) {
+
+
+                    const char *attrType =
+                        attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER
+                        ? "NUM"
+                        : "STR";
+
+
+                    printf("  %s : %s\n",
+                        attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal,
+                        attrType);
+                }
+            }
+
+
+            // Move to next attribute catalog block
+            attrBlock = attrCatHeader.rblock;
+        }
+
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
